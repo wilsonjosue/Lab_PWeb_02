3117b16d8816186a97aba26cb6c41506a22c0f08
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-11 11:11:35 -0500

    Primera resolucion de la del ejercicio1 de la carpeta EjerciciosPropuestos

---
+++ EjerciciosPropuestos/Ejercicio1/JavaScript-01.js
@@ -0,0 +1,14 @@
+function obtenerDiaSemana(numeroDia) {
+    // Obtener la fecha actual
+    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    return diasSemana[numeroDia];
+}
+
+// Obtener la fecha actual
+const fechaActual = new Date();
+// Obtener el número de día de la semana (0 para Domingo, 1 para Lunes, etc.)
+const numeroDia = fechaActual.getDay();
+// Obtener el nombre del día de la semana
+let nombreDia = obtenerDiaSemana(numeroDia);
+
+document.getElementById("resultado").innerText = "Hoy es " + nombreDia;
